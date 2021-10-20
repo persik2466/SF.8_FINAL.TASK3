c186dc186ce8977aec7d6c9a97b333413cf05475
Irina Lokosova <persik2466@gmail.com>
2021-10-20 20:52:01 +0400

    Добавьте файлы проекта.

---
+++ SF.8_FINAL.TASK3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF._8_FINAL.TASK3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
